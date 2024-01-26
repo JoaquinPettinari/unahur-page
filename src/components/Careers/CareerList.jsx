93fece91c71ef9a4a99c5fe8d7e0b73f7ea20df8
JoaquinPettinari <joaquinpettinari@hotmail.com>
2024-01-26 11:03:59 -0300

    Basic cards and island

--- src/components/Careers/CareerList.jsx
+++ src/components/Careers/CareerList.jsx
@@ -1,15 +1,24 @@
 import { useState } from "preact/hooks";
+import { CAREER_LIST } from "../../mockData/careers";
 
-export default function Counter() {
-  const [count, setCount] = useState(0);
+export default function CareerList() {
+  const [activeCard, setActiveCard] = useState(CAREER_LIST[0].id);
 
   return (
-    <div className="counter">
-      <button onClick={() => setCount(count - 1)} disabled={count === 0}>
-        -
-      </button>
-      <h2>{count}</h2>
-      <button onClick={() => setCount(count + 1)}>+</button>
+    <div class="flex gap-6">
+      {CAREER_LIST.map((career, index) => {
+        const { id, icon, name } = career;
+        return (
+          <div
+            class="flex flex-col items-center shadow-xl h-72, w-48 p-4"
+            onClick={() => setActiveCard(id)}
+            key={index}
+          >
+            <img src={icon.src} alt="Icon of career" class="w-16 h-16 mb-2" />
+            <h2 class="text-xl font-bold">{name}</h2>
+          </div>
+        );
+      })}
     </div>
   );
 }
---
+++ src/mockData/careers.ts
@@ -0,0 +1,33 @@
+import VideoGames from "../images/videojuegos-icono.png";
+import InformaticDegree from "../images/Informatica-licenciatura-icono.png";
+import IAIcon from "../images/Inteligencia-artificial-icono.png";
+import NetworkIcon from "../images/redes-icono.png";
+import ProgIcon from "../images/programacion-icono.png";
+
+export const CAREER_LIST = [
+  {
+    name: "Tecnicatura Universitaria en Programación de Videojuegos",
+    id: "tec_uni_prog_videojuegos",
+    icon: VideoGames,
+  },
+  {
+    name: "Tecnicatura Universitaria en Inteligencia Artificial",
+    id: "tec_uni_ia",
+    icon: IAIcon,
+  },
+  {
+    name: "Tecnicatura Universitaria en Programación",
+    id: "tec_uni_prog",
+    icon: ProgIcon,
+  },
+  {
+    name: "Tecnicatura Universitaria en Redes y Operaciones",
+    id: "tec_uni_redes_op",
+    icon: NetworkIcon,
+  },
+  {
+    name: "Licenciatura en Informática",
+    id: "lic_informatica",
+    icon: InformaticDegree,
+  },
+];
