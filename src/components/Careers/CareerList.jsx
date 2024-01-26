71b9d3b1a35defd45eba85c53683e3ab4e39455c
JoaquinPettinari <Joaquinpettinari@hotmail.com>
2024-01-26 17:29:58 -0300

    Cards careers

--- src/components/Careers/CareerList.jsx
+++ src/components/Careers/CareerList.jsx
@@ -1,17 +1,23 @@
-import { useState } from "preact/hooks";
+import { useState, useMemo } from "preact/hooks";
 import { CAREER_LIST } from "../../mockData/careers";
 
 export default function CareerList() {
-  const [activeCard, setActiveCard] = useState(CAREER_LIST[0].id);
+  const [activeCardId, setActiveCardId] = useState(CAREER_LIST[0].id);
+
+  const activeCard = useMemo(() => {
+    return CAREER_LIST?.find((career) => career.id === activeCardId)
+  }, [activeCardId])
 
   return (
-    <div class="flex gap-6">
+    <div class="flex justify-center flex-wrap gap-6">
       {CAREER_LIST.map((career, index) => {
         const { id, icon, name } = career;
         return (
           <div
-            class="flex flex-col items-center shadow-xl h-72, w-48 p-4"
-            onClick={() => setActiveCard(id)}
+            class={`flex flex-col items-center h-72, w-48 p-4 cursor-pointer rounded-2xl ${
+              activeCardId === id ? "shadow-2xl" : ""
+            }`}
+            onClick={() => setActiveCardId(id)}
             key={index}
           >
             <img src={icon.src} alt="Icon of career" class="w-16 h-16 mb-2" />
@@ -19,6 +25,14 @@ export default function CareerList() {
           </div>
         );
       })}
+      <div class="rounded-2xl shadow-xl w-full" >
+        <div class="w-4/4 lg:w-2/4">
+          <p class="primary-color text-xl font-bold">{activeCard.name}</p>
+        </div>
+        <div class="w-4/4 lg:w-2/4">
+
+        </div>
+      </div>
     </div>
   );
 }
