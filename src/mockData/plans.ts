8ff8a4f22bd26e1f0ebe6061b168bf2d83838c7a
JoaquinPettinari <Joaquinpettinari@hotmail.com>
2024-01-29 10:34:59 -0300

    Basic carousel

---
+++ src/mockData/plans.ts
@@ -0,0 +1,43 @@
+import Programacion from "../images/desarrolladores.jpg";
+import RedesOperaciones from "../images/planes-redes-operaciones.jpg";
+import Licenciatura from "../images/licenciatura.jpg";
+import VideoJuegos from "../images/tecnicatura-videojuego-nueva.png";
+import PlanillaComparativa from "../images/planilla-comparativa.jpg";
+
+export const PLANS = [
+  {
+    title: "Tecnicatura Universitaria en Programación",
+    description:
+      "Focalizada en desarrollar el pensamiento computacional y los conocimientos para las necesidades de programación de la actualidad.",
+    image: Programacion,
+  },
+  {
+    title: "Tecnicatura Universitaria en Redes y Operaciones",
+    description:
+      "Orientada a formar profesionales DevOps que forman parte del ecosistema del desarrollo de software.",
+    image: RedesOperaciones,
+  },
+  {
+    title: "Licenciatura en informática",
+    description:
+      "Equivalencias con las Tecnicaturas: acá te mostramos cómo se obtienen materias de la Licenciatura cursando las tecnicaturas.",
+    image: Licenciatura,
+  },
+  {
+    title: "Tecnicatura Universitaria en Inteligencia Artificial",
+    description: "Falta llenar",
+    image: Licenciatura,
+  },
+  {
+    title: "Tecnicatura Universitaria en Programación de Videojuegos",
+    description:
+      "Si te gusta la programación y los videojuegos, podés integrarte a este mundo abordando las problemáticas del diseño y desarrollo.",
+    image: VideoJuegos,
+  },
+  {
+    title: "Planilla Comparativa",
+    description:
+      "Esta planilla de cálculo te permite ver cómo aplican las materias que tenés aprobadas en cada plan",
+    image: PlanillaComparativa,
+  },
+];
